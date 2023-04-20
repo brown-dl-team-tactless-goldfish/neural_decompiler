a9fa78597634848c9564acf886dcb718a2117171
ajy25 <andrew_j_yang@brown.edu>
2023-04-19 22:24:33 -0400

    update leetcode scraper

--- leetcode_scraper_search.py
+++ leetcode_scraper_search.py
@@ -1,31 +1,28 @@
-import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 import time
-import pandas as pd
 import os
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 os.chdir(f"{dir_path}/code")
 
 SLEEP_TIME = 2
-chrome_options = Options()
-chrome_options.add_experimental_option("detach", True)
+service = Service('drivers/chromedriver.exe')
 
-for page in range(1, 54):
+driver_1 = webdriver.Chrome(service=service)
+driver_2 = webdriver.Chrome(service=service)
 
-    driver_1 = webdriver.Chrome('drivers/chromedriver.exe', chrome_options=chrome_options)
-    driver_1.get('https://leetcode.com/problemset/all/?page=' + page)
+for page in range(2, 54):
+
+    
+    driver_1.get(f'https://leetcode.com/problemset/all/?page={page}')
     time.sleep(SLEEP_TIME)
     soup = BeautifulSoup(driver_1.page_source, 'html.parser')
 
     problem_list = soup.find_all('div', {'class': 'truncate'})
     solution_list = []
-
-    df = pd.DataFrame()
 
     for question in problem_list:
         problem_link = question.find('a', href=True).get('href')
@@ -33,7 +30,6 @@ for page in range(1, 54):
 
     for solution_link in solution_list:
 
-        driver_2 = webdriver.Chrome('drivers/chromedriver.exe', chrome_options=chrome_options)
         driver_2.get(solution_link)
         time.sleep(SLEEP_TIME)
         search_bar = driver_2.find_element(By.XPATH, "//input[@type='text']").send_keys('C')
@@ -52,8 +48,11 @@ for page in range(1, 54):
             sub_sub_soup = BeautifulSoup(driver_2.page_source,'lxml')
             time.sleep(SLEEP_TIME)
 
-            text_list = []
-            if sub_sub_soup.find('body').find('div', {'id': '__next'}).find('div', class_='break-words').find('div', class_='mb-6 rounded-lg px-3 py-2.5 font-menlo text-sm bg-fill-3 dark:bg-dark-fill-3'):
-                for code in sub_sub_soup.find('body').find('div', {'id': '__next'}).find('div', class_='break-words').find_all('div', class_='mb-6 rounded-lg px-3 py-2.5 font-menlo text-sm bg-fill-3 dark:bg-dark-fill-3'):
-                    with open(question + '-' + str(i) + '.txt', 'w') as f:
-                        f.write(code.text)
+            try:
+                text_list = []
+                if sub_sub_soup.find('body').find('div', {'id': '__next'}).find('div', class_='break-words').find('div', class_='mb-6 rounded-lg px-3 py-2.5 font-menlo text-sm bg-fill-3 dark:bg-dark-fill-3'):
+                    for code in sub_sub_soup.find('body').find('div', {'id': '__next'}).find('div', class_='break-words').find_all('div', class_='mb-6 rounded-lg px-3 py-2.5 font-menlo text-sm bg-fill-3 dark:bg-dark-fill-3'):
+                        with open(question + '-' + str(i) + '.txt', 'w') as f:
+                            f.write(code.text)
+            except:
+                pass
