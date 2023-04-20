ad55e2a2961addb44310d07372c0327610bac29a
ajy25 <andrew_j_yang@brown.edu>
2023-04-19 23:22:44 -0400

    removed cpp

--- cpp_remover.py
+++ cpp_remover.py
@@ -1,16 +1,23 @@
 import os
-import sys
+import shutil
 
 
 current_file_path = os.path.dirname(os.path.realpath(__file__))
-code_dir_path = f"{current_file_path}/"
+code_dir_path = f"{current_file_path}/code"
+
+
 
 for file_name in sorted(list(os.listdir(code_dir_path))): 
     path = f"{code_dir_path}/{file_name}"
 
-    print(path)
-
     with open(path, "r") as f:
-        c_code = f.read()
-        "class Solution"
-        if 
+        try:
+
+            c_code = f.read()
+            cpp_unique_str = "class Solution"
+            if cpp_unique_str in c_code:
+                shutil.move(path, f"cpp_code/{file_name}")
+        
+        except:
+            shutil.move(path, f"cpp_code/{file_name}")
+
