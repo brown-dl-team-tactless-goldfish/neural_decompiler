0f960021c60776ab315e1f10282dd8a42d5ae7be
frankchiu12 <si-un_chiu@brown.edu>
2023-04-19 23:25:09 -0400

    regex

--- clean.py
+++ clean.py
@@ -5,6 +5,6 @@ with open('test.txt') as f:
 
 lines = re.sub(r'\/\*[\s\S]*?\*\/', '', lines)
 lines = re.sub(r'//.*\n', '\n', lines)
-lines = re.sub(r'\s+', ' ', lines)
+lines = re.sub(r'\s+', ' ', lines).strip()
 
 print(lines)
