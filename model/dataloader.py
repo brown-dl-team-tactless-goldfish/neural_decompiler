6a12dabf478e77eca098807e3056ea0bd2b2ced6
ajy25 <andrew_j_yang@brown.edu>
2023-04-26 23:49:48 -0400

    add toy dataset, begin model

---
+++ model/dataloader.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+
+
+
+
+
+
+
+
+class DataLoader:
+    """
+    A class that loads and preprocesses training data
+    """
+    
+    c_path = None
+    asm_path = None
+
+    def __init__(self, c_path, asm_path):
+        self.c_path = c_path
+        self.asm_path = asm_path
+
+    def generate_c_vocabulary(self):
+        return
+    
+    def generate_asm_vocabulary(self):
+        return
+
+    
