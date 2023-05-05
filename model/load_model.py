5e1d696330d8b2e1e921acf41ade97d065c7a7ab
frankchiu12 <si-un_chiu@brown.edu>
2023-05-05 19:27:05 -0400

    load model sad

--- model/load_model.py
+++ model/load_model.py
@@ -1,48 +1,32 @@
 import tensorflow as tf
 import os
-from neural_decompiler import NeuralDecompiler, CGenerator
+from neural_decompiler import *
 from transformer.dataprocess import read_vocab_from_csv
+import json
 
-# TypeError: __init__() missing 6 required positional arguments: 'emb_sz', 'input_vocab_size', 'output_vocab_size', 'ff_hidden_dim', 'num_layers', and 'num_heads'
+# f = open('model_checkpoints/model-checkpoint-metadata.json')
+# param_data = json.load(f)
 
+with open('data/ASM_tests/cs300midterm_q3.txt', 'r') as f:
+    test_data = f.read()
 
-current_dir = os.path.dirname(os.path.realpath(__file__))
+# emb_sz = param_data['emb_sz']
+# input_vocab_size = param_data['input_vocab_size']
+# output_vocab_size = param_data['output_vocab_size']
+# ff_hidden_dim = param_data['ff_hidden_dim']
+# num_layers = param_data['num_layers']
+# num_heads = param_data['num_heads']
 
+# asm_tokenized, _, _ = tokenize_asm(test_data)
+# asm_vocab = read_vocab_from_csv('model/vocab/new_asm_vocab.csv')
+# tensor = generate_tensor_from_vocab(asm_vocab, asm_tokenized)
 
+# model = NeuralDecompiler()
+# model.load_weights('drive_download/checkpoint')
 
-
-
-asm_vocab_dir = "vocab/new_asm_vocab.csv"
-c_vocab_dir = "vocab/new_c_vocab.csv"
-
-asm_vocab_path = f"{current_dir}/{asm_vocab_dir}"
-c_vocab_path = f"{current_dir}/{c_vocab_dir}"
-
-ASM_VOCAB = read_vocab_from_csv(asm_vocab_path)
-C_VOCAB = read_vocab_from_csv(c_vocab_path)
-
-
-
-
-
-model = NeuralDecompiler()
-model.load_weights('drive_download/checkpoint')
-print(model.summary())
-
+model = tf.saved_model.load('model_checkpoints/model-checkpoint')
+out = model.call(test_data).numpy()
+print(out)
 
 
-
-cgen = CGenerator(model, ASM_VOCAB, C_VOCAB)
-
-
-
-
-
-def generate_c(n_dcmp, asm_test_file_path):
-    with open(asm_test_file_path, "r") as f:
-        asm_code = f.read()
-    return cgen(asm_code, 100)
-
-
-
-
+# print(model.summary())
